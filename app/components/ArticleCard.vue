9e4cdade40850e60612523c23bcbc3022abb4bb8
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-08-22 15:39:43 +0200

    added article cards

---
+++ app/components/ArticleCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="bg-white dark:bg-gray-800 rounded-2xl shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300"
+  >
+    <img :src="image" class="w-full h-48 object-cover" />
+    <div class="p-4">
+      <h2 class="text-xl font-bold text-gray-900 dark:text-white mb-2">
+        {{ title }}
+      </h2>
+      <p class="text-sm text-gray-500 dark:text-gray-400 mb-2">
+        {{ author }} • {{ date }}
+      </p>
+      <p class="text-gray-700 dark:text-gray-300 leading-relaxed">
+        {{ excerpt }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  author: string;
+  date: string;
+  image: string;
+  excerpt: string;
+}>();
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,54 @@
 <template>
-  <div>
-    <h1>Strona główna</h1>
+  <div class="min-h-screen bg-primary">
+    <div class="flex justify-center p-4 bg-primary">
+      <div class="max-w-3xl bg-white rounded-2xl shadow-lg p-6 space-y-4">
+        <h1 class="text-3xl font-bold text-gray-900 dark:text-white">
+          Strona główna
+        </h1>
+
+        <!-- Статьи -->
+        <section
+          class="max-w-5xl mx-auto p-4 grid gap-6 md:grid-cols-2 lg:grid-cols-3"
+        >
+          <ArticleCard
+            v-for="(article, index) in articles"
+            :key="index"
+            :title="article.title"
+            :author="article.author"
+            :date="article.date"
+            :image="article.image"
+            :excerpt="article.excerpt"
+          />
+        </section>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import ArticleCard from "/Users/polinabezukladnova/Desktop/animals_app/app/components/ArticleCard.vue";
+
+const articles = [
+  {
+    title: "Artykuł 1",
+    author: "Autor 1",
+    date: "22 sierpnia 2025",
+    image: "/images/dog-article1.jpg",
+    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    title: "Artykuł 2",
+    author: "Autor 2",
+    date: "21 kwietnia 2025",
+    image: "/images/cat-article2.jpg",
+    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    title: "Artykuł 3",
+    author: "Autor 3",
+    date: "20 lutego 2025",
+    image: "/images/parrot-article3.jpg",
+    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+];
+</script>
