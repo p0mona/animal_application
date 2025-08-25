05ee1ffcc0b43513f51759f1de5d5d5942eb7b5f
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-08-25 11:30:32 +0200

    added calendar to to tracker

--- app/pages/calendar.vue
+++ app/pages/calendar.vue
@@ -2,90 +2,112 @@
   <div class="min-h-screen bg-primary">
     <div class="flex justify-center p-4 bg-primary">
       <div class="max-w-3xl bg-white rounded-2xl shadow-lg p-6 space-y-4">
-        <h1 class="text-3xl font-bold text-gray-900 dark:text-white">
-          Tracker wagi i wzrostu
-        </h1>
-
-        <!-- Фальшивый график -->
-        <svg class="w-full h-32 bg-gray-100 rounded-lg" viewBox="0 0 100 32">
-          <!-- Линия веса -->
-          <polyline
-            points="0,28 10,26 20,24 30,22 40,23 50,20 60,18 70,19 80,17 90,15 100,16"
-            fill="none"
-            stroke="mediumpurple"
-            stroke-width="2"
-          />
-          <!-- Линия роста -->
-          <polyline
-            points=2
-            fill="none"
-            stroke="orchid"
-            stroke-width="2"
-          />
-        </svg>
-
-        <!-- Форма для ввода -->
-        <form @submit.prevent="addEntry" class="space-y-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div>
-            <label class="block text-sm font-semibold">Data:</label>
-            <input
-              type="date"
-              v-model="newEntry.date"
-              class="border p-2 rounded w-full"
-              required
-            />
-          </div>
-          <div>
-            <label class="block text-sm font-semibold">Waga (kg):</label>
-            <input
-              type="number"
-              v-model.number="newEntry.weight"
-              class="border p-2 rounded w-full"
-              required
-            />
-          </div>
-          <div>
-            <label class="block text-sm font-semibold">Wzrost (cm):</label>
-            <input
-              type="number"
-              v-model.number="newEntry.height"
-              class="border p-2 rounded w-full"
-              required
-            />
-          </div>
-          <UButton type="submit">Dodaj</UButton>
-        </form>
-
-        <!-- История записей -->
-        <div v-if="entries.length" class="space-y-4">
-          <h2 class="font-semibold text-lg">Historia:</h2>
-
-          <!-- Список с данными -->
-          <ul class="divide-y">
-            <li
-              v-for="(entry, index) in entries"
-              :key="index"
-              class="py-2 flex justify-between items-center"
+            <h3
+              class="text-xl font-semibold text-gray-900 dark:text-white mb-4"
             >
-              <span>{{ entry.date }}</span>
-              <div class="flex-1 ml-4 space-y-1">
-                <div class="text-sm">Waga: {{ entry.weight }} kg</div>
-                <div class="w-full bg-gray-200 rounded h-2">
-                  <div
-                    class="bg-[#9370DB] h-2 rounded"
-                    :style="{ width: (entry.weight / maxWeight) * 100 + '%' }"
-                  ></div>
-                </div>
-                <div class="text-sm">Wzrost: {{ entry.height }} cm</div>
-                <div class="w-full bg-gray-200 rounded h-2">
-                  <div
-                    class="bg-[#DA70D6] h-2 rounded"
-                    :style="{ width: (entry.height / maxHeight) * 100 + '%' }"
-                  ></div>
-                </div>
+              Przypomnienia
+            </h3>
+            <UCalendar multiple />
+          </div>
+
+          <div>
+            <h3
+              class="text-xl font-semibold text-gray-900 dark:text-white mb-4"
+            >
+              Tracker wagi i wzrostu
+            </h3>
+
+            <!-- Фальшивый график -->
+            <svg
+              class="w-full h-32 bg-gray-100 rounded-lg"
+              viewBox="0 0 100 32"
+            >
+              <!-- Линия веса -->
+              <polyline
+                points="0,28 10,26 20,24 30,22 40,23 50,20 60,18 70,19 80,17 90,15 100,16"
+                fill="none"
+                stroke="mediumpurple"
+                stroke-width="2"
+              />
+              <!-- Линия роста -->
+              <polyline
+                points="2"
+                fill="none"
+                stroke="orchid"
+                stroke-width="2"
+              />
+            </svg>
+
+            <!-- Форма для ввода -->
+            <form @submit.prevent="addEntry" class="space-y-4">
+              <div>
+                <label class="block text-sm font-semibold">Data:</label>
+                <input
+                  type="date"
+                  v-model="newEntry.date"
+                  class="border p-2 rounded w-full"
+                  required
+                />
               </div>
-            </li>
-          </ul>
+              <div>
+                <label class="block text-sm font-semibold">Waga (kg):</label>
+                <input
+                  type="number"
+                  v-model.number="newEntry.weight"
+                  class="border p-2 rounded w-full"
+                  required
+                />
+              </div>
+              <div>
+                <label class="block text-sm font-semibold">Wzrost (cm):</label>
+                <input
+                  type="number"
+                  v-model.number="newEntry.height"
+                  class="border p-2 rounded w-full"
+                  required
+                />
+              </div>
+              <UButton type="submit">Dodaj</UButton>
+            </form>
+
+            <!-- История записей -->
+            <div v-if="entries.length" class="space-y-4">
+              <h2 class="font-semibold text-lg">Historia:</h2>
+
+              <!-- Список с данными -->
+              <ul class="divide-y">
+                <li
+                  v-for="(entry, index) in entries"
+                  :key="index"
+                  class="py-2 flex justify-between items-center"
+                >
+                  <span>{{ entry.date }}</span>
+                  <div class="flex-1 ml-4 space-y-1">
+                    <div class="text-sm">Waga: {{ entry.weight }} kg</div>
+                    <div class="w-full bg-gray-200 rounded h-2">
+                      <div
+                        class="bg-[#9370DB] h-2 rounded"
+                        :style="{
+                          width: (entry.weight / maxWeight) * 100 + '%',
+                        }"
+                      ></div>
+                    </div>
+                    <div class="text-sm">Wzrost: {{ entry.height }} cm</div>
+                    <div class="w-full bg-gray-200 rounded h-2">
+                      <div
+                        class="bg-[#DA70D6] h-2 rounded"
+                        :style="{
+                          width: (entry.height / maxHeight) * 100 + '%',
+                        }"
+                      ></div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
