c5f9ef57e0122539943a379406c79ce15eb42142
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-08-25 11:44:33 +0200

    added fileupload

--- app/pages/documents.vue
+++ app/pages/documents.vue
@@ -5,6 +5,23 @@
         <h1 class="text-3xl font-bold text-gray-900 dark:text-white">
           Dokumenty
         </h1>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div class="underline cursor-pointer">
+            <ul>
+              Książka zdrowia
+            </ul>
+            <ul>
+              Paszport
+            </ul>
+            <ul>
+              Metryka
+            </ul>
+          </div>
+
+          <div>
+            <UFileUpload label="Przeciągnij plik tutaj" class="w-full min-h-48 max-w-md" />
+          </div>
+        </div>
       </div>
     </div>
   </div>
