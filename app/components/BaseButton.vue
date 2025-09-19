2699d8111a51a81547ccba6e311dfc71cb4aa47c
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-09-19 09:36:41 +0200

    add cursor-pointer for base button

--- app/components/BaseButton.vue
+++ app/components/BaseButton.vue
@@ -1,7 +1,7 @@
 <template>
   <UButton
     type="button"
-    class="md:w-auto bg-violet-500 hover:bg-violet-600 active:bg-violet-700"
+    class="md:w-auto bg-violet-500 hover:bg-violet-600 active:bg-violet-700 cursor-pointer"
   >
     {{ label }}
   </UButton>
