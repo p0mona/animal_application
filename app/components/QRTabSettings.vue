8359b4bd0b1d87283cf0ca61c3c9e70fa2391754
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-09-19 14:29:58 +0200

    add button for uploading qr

--- app/components/QRTabSettings.vue
+++ app/components/QRTabSettings.vue
@@ -5,7 +5,15 @@
     </template>
     <div class="w-full flex flex-col items-center">
       <img src="/images/qr.svg" class="mb-4" />
-      <BaseButton label="Generuj QR" />
+      <dev class="space-x-4">
+        <BaseButton label="Generuj QR" />
+        <UButton
+          type="button"
+          class="md:w-auto border border-violet-500 bg-white text-violet-500 hover:border-violet-600 active:border-violet-700 hover:text-violet-600 active:text-violet-700 hover:bg-white active:bg-white cursor-pointer"
+        >
+          Pobrać
+        </UButton>
+      </dev>
     </div>
   </UCard>
 </template>
