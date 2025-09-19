319982f303393f8962d37adf5ae16975fec0413b
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-09-19 13:43:54 +0200

    remove unnecessary imports

--- app/components/BaseCheckbox.vue
+++ app/components/BaseCheckbox.vue
@@ -8,7 +8,6 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps, defineEmits } from "vue";
 
 const props = defineProps<{
   label: string;
--- app/components/FormVisitSection.vue
+++ app/components/FormVisitSection.vue
@@ -12,7 +12,6 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps } from "vue";
 
 defineProps({
   title: { type: String, required: true },
