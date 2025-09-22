17746a336ab0f6cda09d65def5b708a9688e4148
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-09-22 18:42:13 +0200

    add UI for reminder form with date, type and actions

--- app/pages/reminders.vue
+++ app/pages/reminders.vue
@@ -6,7 +6,28 @@
       <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
         <div class="mt-4">
           <Calendar v-model="selectedDate" />
+          <div class="p-4 space-y-4">
+            <h3 class="text-lg font-semibold">Dodać przypomnienie</h3>
+            <p class="text-sm">Wybrana data: {{ formatDate(selectedDate) }}</p>
+
+            <RadioButton :items="types" v-model="form.type" />
+
+            <!-- <BaseInput label="Wpisz tytuł przypomnienia" v-model="form.title"/> -->
+
+            <div class="flex justify-end space-x-2">
+              <UButton
+                type="button"
+                @click="closeModal"
+                class="md:w-auto border border-violet-500 bg-white text-violet-500 hover:border-violet-600 active:border-violet-700 hover:text-violet-600 active:text-violet-700 hover:bg-white active:bg-white cursor-pointer"
+              >
+                Anuluj
+              </UButton>
+
+              <BaseButton label="Zapisz" @click="saveEvent"/>
+            </div>
+          </div>
         </div>
+
         <div class="space-y-2 mt-4 ml-4">
           <h5 class="font-semibold">Najbliższe szczepienie:</h5>
           <p>Termin: -----</p>
@@ -28,5 +49,17 @@
 
 <script setup lang="ts">
 import Calendar from "~/components/Calendar.vue";
+import RadioButton from "~/components/RadioButton.vue";
+
 const selectedDate = ref<any>(null);
+const form = ref({
+  title: "",
+  type: "",
+  details: ""
+});
+const types = ref<RadioGroupItem[]>([
+  { label: "Szczepionka", value: "vaccination" },
+  { label: "Terapia przeciwpasożytnicza", value: "therapy" },
+  { label: "Wizyta", value: "visit" },
+]);
 </script>
