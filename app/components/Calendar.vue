76027e83c7c49ee0f999819c6487bb50661c62a3
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-09-22 18:32:53 +0200

    add v-model support and mount control

--- app/components/Calendar.vue
+++ app/components/Calendar.vue
@@ -1,14 +1,47 @@
 <template>
-  <div class="mt-4">
+  <div class="mt-4" v-if="isMounted">
     <UCalendar
-      multiple
+      v-model="innerValue"
       :ui="calendarUI"
       class="[&_thead_th]:text-violet-600 [&_tr_th]:text-violet-600"
+      @update:model-value="updateValue"
     />
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, watch, onMounted, onUnmounted } from "vue";
+
+const props = defineProps<{
+  modelValue: any;
+}>();
+
+const emit = defineEmits<{
+  (e: "update:modelValue", value: any): void;
+}>();
+
+const isMounted = ref(false);
+const innerValue = ref(props.modelValue);
+
+onMounted(() => {
+  isMounted.value = true;
+});
+
+onUnmounted(() => {
+  isMounted.value = false;
+});
+
+watch(
+  () => props.modelValue,
+  (val) => {
+    innerValue.value = val;
+  }
+);
+
+const updateValue = (val: any) => {
+  emit("update:modelValue", val);
+};
+
 const calendarUI = {
   cellTrigger: `
     w-8 h-8 rounded-full
