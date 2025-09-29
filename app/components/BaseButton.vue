52d1f31c77b4d8909f2fb079cbd08c4aee393dfc
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-09-29 21:13:21 +0200

    update styles for button

--- app/components/BaseButton.vue
+++ app/components/BaseButton.vue
@@ -1,7 +1,7 @@
 <template>
   <UButton
     type="button"
-    class="md:w-auto bg-violet-500 hover:bg-violet-600 active:bg-violet-700 cursor-pointer"
+    class="md:w-auto bg-violet-500 hover:bg-violet-600 active:bg-violet-700 cursor-pointer aria-disabled:bg-violet-600"
   >
     {{ label }}
   </UButton>
--- app/components/BorderButton.vue
+++ app/components/BorderButton.vue
@@ -1,7 +1,7 @@
 <template>
   <UButton
     type="button"
-    class="md:w-auto border border-violet-500 bg-white text-violet-500 hover:border-violet-600 active:border-violet-700 hover:text-violet-600 active:text-violet-700 hover:bg-white active:bg-white cursor-pointer"
+    class="md:w-auto border border-violet-500 bg-white text-violet-500 hover:border-violet-600 active:border-violet-700 hover:text-violet-600 active:text-violet-700 hover:bg-white active:bg-white cursor-pointer aria-disabled:bg-white aria-disabled:border-violet-600"
   >
     {{ label }}
   </UButton>
