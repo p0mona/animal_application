17516b3bcb7d01ed2a3f1c0086c723ddfb527a67
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-09-29 12:42:47 +0200

    update style for qr module

--- app/components/ProfileTabSettings.vue
+++ app/components/ProfileTabSettings.vue
@@ -53,7 +53,7 @@
       <div class="bg-white p-6 rounded-2xl shadow-xl relative">
         <button
           @click="showQRModal = false"
-          class="absolute top-2 right-2 text-gray-600 hover:text-black"
+          class="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
         >
           ✖
         </button>
