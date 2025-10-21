c47c464128e9d745a2a6e9bf9fd5aa8e68556f10
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-10-21 17:27:29 +0200

    update ui for new_appointment

--- app/pages/new_appointment.vue
+++ app/pages/new_appointment.vue
@@ -3,17 +3,25 @@
     <BackButton to="/patient" />
 
     <h1 class="mb-5 text-2xl font-bold">Nowa wizyta</h1>
-    <div class="space-y-4">
-      <BaseInput label="Termin" id="date" type="date" v-model="form.date" />
-      <BaseInput
-        label="Godzina"
-        id="oclock"
-        v-model="form.oclock"
-        type="time"
-      />
-      <BaseInput label="Cel" v-model="form.object" />
-
-      <BaseButton label="Zapisz" />
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div class="space-y-2">
+        <BaseInput label="Imię pupila" v-model="form.animal_name" />
+        <BaseInput label="Imię właściciela" v-model="form.name" />
+        <BaseInput label="Kontakt" v-model="form.contact" />
+      </div>
+      <div class="space-y-2">
+        <BaseInput label="Termin" id="date" type="date" v-model="form.date" />
+        <BaseInput
+          label="Godzina"
+          id="oclock"
+          v-model="form.oclock"
+          type="time"
+        />
+        <BaseInput label="Cel" v-model="form.object" />
+        <div class="flex justify-end mt-4">
+          <BaseButton label="Zapisz" />
+        </div>
+      </div>
     </div>
   </Layout>
 </template>
@@ -23,5 +31,8 @@ const form = ref({
   birthday: "",
   oclock: "",
   object: "",
+  animal_name: "",
+  name: "",
+  contact: "",
 });
 </script>
